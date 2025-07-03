f2571e2641bd1bf79fd725e76a089c4e2e8e9aaa
andrew <andrew@housermail.com>
2025-07-02 22:48:48 -0400

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import NavBar from '../lib/components/NavBar.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="min-h-screen bg-neutral-400 px-8 pt-4">
+	<NavBar />
+	{@render children()}
+</div>
