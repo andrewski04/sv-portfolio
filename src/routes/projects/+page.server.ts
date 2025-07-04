3859c5f53a5d6e168f82baaedc621ab2c627958f
andrew <andrew@housermail.com>
2025-07-04 15:06:36 -0400

    bug fix

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -3,7 +3,7 @@ import { read } from '$app/server';
 import YAML from 'yaml';
 
 export const load = (async () => {
-	const projects = await read('$content/pages/projects.yaml').text();
+	const projects = await read('/content/pages/projects.yaml').text();
 	const projectsData = YAML.parse(projects);
 	return { projects: projectsData };
 }) satisfies PageServerLoad;
--- src/routes/resume/+page.server.ts
+++ src/routes/resume/+page.server.ts
@@ -3,7 +3,7 @@ import { read } from '$app/server';
 import YAML from 'yaml';
 
 export const load = (async () => {
-	const resume = await read('$content/pages/resume.yaml').text();
+	const resume = await read('/content/pages/resume.yaml').text();
 	const resumeData = YAML.parse(resume);
 	return { resume: resumeData };
 }) satisfies PageServerLoad;
